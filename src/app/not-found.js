57363495451b65c191e634a3b8895539bf942157
Nesse-org <jy288794@gmail.com>
2026-05-10 13:33:46 +0500

    not found bor

---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+
+    return (
+        <div style="container">
+            <h1 style="heading">404 - Page Not Found</h1>
+            <p style="paragraph">Sorry, the page you are looking for does not exist.</p>
+        </div>
+    );
+};
+
+export default NotFound;
